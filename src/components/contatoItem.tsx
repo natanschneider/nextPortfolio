878e89f4dd780f0e7e66e5cf1ad7cdeb78b07ab4
natanschneider <natanschneider@protonmail.com>
2023-10-26 14:40:14 -0300

    contatoItem initial component

---
+++ src/components/contatoItem.tsx
@@ -0,0 +1,28 @@
+// import Image from "next/image";
+
+export default function ContatoItem({ nome, description, address, icon }: any) {
+  return (
+    <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6 md:mr-16">
+      <a href={address} target="_blank" rel="noopener noreferrer">
+        <div className="flex items-start">
+          <div className="shrink-0">
+            <div className="p-4 bg-blue-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+              { /* <Image 
+                src={icon} 
+                alt={'icone'}
+                height={10}
+                width={10}
+              /> 
+                to-do figure out how to render this svg
+              */}
+            </div>
+          </div>
+          <div className="grow ml-6">
+            <p className="font-bold mb-1">{nome}</p>
+            <p className="text-gray-500">{description}</p>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
