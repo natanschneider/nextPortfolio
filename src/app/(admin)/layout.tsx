35bc64d6a9026001a930c27a30405615c9d20703
natanschneider <natanschneider@protonmail.com>
2023-11-01 10:04:08 -0300

    add layout for admin routes

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/app/globals.css";
+import Header from "@/components/home/header";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Admin || Natãn Schneider",
+  description: "Admin panel",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>
+        <div className="bg-gray-900 text-stone-300 min-h-screen">
+          <Header />
+          <div className="max-w-5xl w-11/12 mx-auto">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
